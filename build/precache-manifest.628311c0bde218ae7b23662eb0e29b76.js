d5028fe68495faa51cb91eb6b55133d30e9511a3
ヌルポ <episodes27@gmail.com>
2020-08-07 22:58:14 +1000

    update the build

---
+++ build/precache-manifest.628311c0bde218ae7b23662eb0e29b76.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695e634816c6bcb78f99efaef52572ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d835c7be1c91cdf1fbda",
+    "url": "/static/css/2.3979b7dc.chunk.css"
+  },
+  {
+    "revision": "fbc4a7e0adc12676a1ff",
+    "url": "/static/css/main.dd2418c1.chunk.css"
+  },
+  {
+    "revision": "d835c7be1c91cdf1fbda",
+    "url": "/static/js/2.220cf09b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.220cf09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc4a7e0adc12676a1ff",
+    "url": "/static/js/main.b177a0c1.chunk.js"
+  },
+  {
+    "revision": "c2bb69b16817858c364d",
+    "url": "/static/js/runtime-main.4570daff.js"
+  },
+  {
+    "revision": "389d2075267f95091133f787dc01af00",
+    "url": "/static/media/bk.389d2075.jpg"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
